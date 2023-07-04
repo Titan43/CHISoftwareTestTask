1fd42753c08bcf410dee470bd560cc1c2915495f
Titan43 <ukrplay73@gmail.com>
2023-07-04 15:34:11 +0300

    implement UserService

--- src/main/java/com/chiSoftware/testTask/entities/user/UserRepository.java
+++ src/main/java/com/chiSoftware/testTask/entities/user/UserRepository.java
@@ -3,6 +3,9 @@ package com.chiSoftware.testTask.entities.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
 }
--- src/main/java/com/chiSoftware/testTask/serviceProviders/UserServiceProvider.java
+++ src/main/java/com/chiSoftware/testTask/serviceProviders/UserServiceProvider.java
@@ -1,15 +1,26 @@
 package com.chiSoftware.testTask.serviceProviders;
 
 import com.chiSoftware.testTask.entities.user.User;
+import com.chiSoftware.testTask.entities.user.UserRepository;
 import com.chiSoftware.testTask.security.AuthRequest;
 import com.chiSoftware.testTask.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class UserServiceProvider implements UserService {
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public ResponseEntity<?> authenticate(AuthRequest request) {
@@ -18,13 +29,26 @@ public class UserServiceProvider implements UserService {
 
     @Override
     public ResponseEntity<String> createUser(User user) {
+        if(user.getUsername()==null||user.getUsername().strip().equals("")){
+            return new ResponseEntity<>("Username cannot be empty", HttpStatus.BAD_REQUEST);
+        }
+        else if(user.getPassword()==null||user.getPassword().strip().equals("")){
+            return new ResponseEntity<>("Username cannot be empty", HttpStatus.BAD_REQUEST);
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
 
-        return null;
+        return new ResponseEntity<>("User was successfully created", HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<String> deleteUser(Principal principal) {
-        return null;
+        Optional<User> optionalUser = userRepository.findUserByUsername(principal.getName());
+        if(optionalUser.isEmpty()){
+            return new ResponseEntity<>("User does not exist", HttpStatus.NOT_FOUND);
+        }
+        userRepository.delete(optionalUser.get());
+        return new ResponseEntity<>("User was successfully created", HttpStatus.NO_CONTENT);
     }
 
 }
